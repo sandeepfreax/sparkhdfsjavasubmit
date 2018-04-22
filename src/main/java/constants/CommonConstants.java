f777fccd440c0af97d2aadd1876d7fb085b65f24
sandeepfreax <sandeep.freax@gmail.com>
2018-04-22 23:41:58 +0530

    Initial changes for windows setup

---
+++ src/main/java/constants/CommonConstants.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class CommonConstants {
+    public static final String HADOOP_HOME = "C:\\bigdata\\configFiles\\hadoop-2.7.3";
+    public static final String JAVA_HOME = "C:\\Program Files\\Java\\jdk1.8.0_92";
+    public static final String SPARK_HOME = "C:\\bigdata\\configFiles\\spark-2.2.0";
+    public static final String FIRST_JOB_PATH = "...";
+    public static final String MAIN_CLASS_FIRST_JOB = "...";
+    public static final String[] FIRST_JOB_ARGUMENTS = new String[]{"Hello World!!!"};
+    public static final String MASTER_NAME = "local";
+    public static final String DRIVER_MEMORY_FIRST_JOB = "1g";
+}
---
+++ src/main/java/jobs/SubmitSparkJob.java
@@ -0,0 +1,37 @@
+package jobs;
+
+import constants.CommonConstants;
+import org.apache.log4j.Logger;
+import org.apache.spark.launcher.SparkLauncher;
+
+public class SubmitSparkJob {
+    private static final Logger logger = Logger.getLogger(SubmitSparkJob.class);
+
+    public static void main(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+        logger.info("Job started at : " + startTime);
+        submitJob();
+        long endTime = System.currentTimeMillis() - startTime;
+        logger.info("Job ended at : " + endTime);
+        logger.info("Job took : " + endTime/1000 + " seconds");
+    }
+
+    private static void submitJob() throws Exception {
+        logger.info("Starting submitJob");
+        SparkLauncher sparkLauncher = new SparkLauncher()
+                .setVerbose(true)
+                .setJavaHome(CommonConstants.JAVA_HOME)
+                .setSparkHome(CommonConstants.SPARK_HOME)
+                .setAppResource(CommonConstants.FIRST_JOB_PATH)
+                .setMainClass(CommonConstants.MAIN_CLASS_FIRST_JOB)
+                .setMaster(CommonConstants.MASTER_NAME)
+                .setConf(SparkLauncher.DRIVER_MEMORY, CommonConstants.DRIVER_MEMORY_FIRST_JOB)
+                .addAppArgs(CommonConstants.FIRST_JOB_ARGUMENTS);
+
+        logger.info("Launching the spark application");
+        Process process = sparkLauncher.launch();
+        logger.info("Spark Application launched successfully. Waiting for exit code.");
+        int exitCode = process.waitFor();
+        logger.info("Job finished with exit status : " + exitCode);
+    }
+}
